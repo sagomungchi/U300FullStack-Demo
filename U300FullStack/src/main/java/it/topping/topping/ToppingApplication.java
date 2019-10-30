a3db91b24afef98192df181680e67e04b7462114
dailyworker <seansin@chungbuk.ac.kr>
2019-10-30 23:00:07 +0900

    first commit

---
+++ U300FullStack/src/main/java/it/topping/topping/ToppingApplication.java
@@ -0,0 +1,13 @@
+package it.topping.topping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToppingApplication.class, args);
+    }
+
+}
---
+++ U300FullStack/src/test/java/it/topping/topping/ToppingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.topping.topping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
