86f44baad7312cf2414b116b2c56364c3e33a83e
dailyworker <seansin@chungbuk.ac.kr>
2019-10-31 17:22:14 +0900

    reslove #1 by creating CustomException Handler

--- U300FullStack/src/main/java/it/topping/topping/services/ProjectService.java
+++ U300FullStack/src/main/java/it/topping/topping/services/ProjectService.java
@@ -1,6 +1,7 @@
 package it.topping.topping.services;
 
 import it.topping.topping.domain.Project;
+import it.topping.topping.exceptions.ProjectIdException;
 import it.topping.topping.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID'" + project.getProjectIdentifier().toUpperCase()+"' already access");
+        }
     }
-
-
 }
